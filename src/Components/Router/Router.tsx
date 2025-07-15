890c566f83ed23cdf8cab7f3f0fcaef34c7ae8b8
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 00:06:42 +0900

    chore: change path

--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Main from '../../page/main/Main';
+import Main from '../../page/main';
 // import Login from '../../page/Login/loginPage'
 // import Profile from '../../page/Profile/ProfilePage';
 // import SpecDetail from "../Specification/SpecDetail";
