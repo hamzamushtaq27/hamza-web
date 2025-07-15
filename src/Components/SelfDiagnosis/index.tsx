79c0e2f00c40a8c976d9df333eda1e242add1e8c
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 00:08:19 +0900

    feat: SelfDiagnosis page

---
+++ src/Components/SelfDiagnosis/index.tsx
@@ -0,0 +1,11 @@
+
+const SelfDiagnosis = () => {
+  return (
+    <div>
+      <h1>Self Diagnosis Page</h1>
+      <p>This is the self diagnosis page content.</p>
+    </div>
+  );
+}
+
+export default SelfDiagnosis;
