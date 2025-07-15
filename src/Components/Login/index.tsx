3b05cd989d13889fd5ae0ff3f186f1cec88e23a5
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 00:47:18 +0900

    feat: login page

---
+++ src/Components/Login/index.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>This is the login page content.</p>
+        </div>
+    )
+}
+
+export default Login;
