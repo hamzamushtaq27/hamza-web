a56c6b0d1a86c1c36de1859bcde08cd56e445664
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 01:48:17 +0900

    feat: logout

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,11 +1,19 @@
+// import * as S from "./style";
 
 const Profile = () => {
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    window.location.replace("/login");
+  };
+
   return (
     <div>
       <h1>Profile Page</h1>
       <p>This is the profile page content.</p>
+      <button onClick={handleLogout}>로그아웃</button>
     </div>
   );
-}
+};
 
 export default Profile;
