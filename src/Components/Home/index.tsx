5ce51232e330267c8afb66c3d70b2297c431ae1c
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 00:07:30 +0900

    feat: Home page

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './style';
+
+const Home = () => {
+
+    return (
+        <S.HomeContainer>
+            <h1>Home</h1>
+            <p>Welcome to the home page!</p>
+        </S.HomeContainer>
+    );
+}
+
+export default Home;
