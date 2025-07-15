afba9cd7e416ca8875008e72471d62cd220a8838
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-16 00:06:19 +0900

    feat: nav bar

---
+++ src/page/main/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Navigation from "../../Components/Navigation";
+import Home from "../../Components/Home";
+import Profile from "../../Components/Profile";
+import SelfDiagnosis from "../../Components/SelfDiagnosis";
+import AiConsultation from "../../Components/aiConsultation";
+import * as S from './style';
+
+const Main = () => {
+  const [selectedMenuNumber, setSelectedMenuNumber] = useState<number>(1);
+
+  const renderMainContent = () => {
+    switch (selectedMenuNumber) {
+      case 1:
+        return <Home />;
+      case 2:
+        return <SelfDiagnosis />;
+      case 3:
+        return <AiConsultation />;
+      case 4:
+        return <Profile />;
+      default:
+        return <Home />;
+    }
+  };
+
+  const handleMenuSelect = (menuNumber: number) => {
+    setSelectedMenuNumber(menuNumber);
+  };
+  return (
+    <div>
+      <S.MainContent>{renderMainContent()}</S.MainContent>
+      <Navigation onMenuSelect={handleMenuSelect} />
+    </div>
+  );
+};
+
+export default Main;
